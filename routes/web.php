a269df447a5cbf65e8efa9127626175bc7c4f6be
Micah Tidwell <micahtidwell25@gmail.com>
2023-10-30 18:17:00 -0500

    Init

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/about', function () {
+    echo '<h1> This is the about page </h1>';
+});
